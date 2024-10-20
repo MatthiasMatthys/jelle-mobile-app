a158d84d85cc35b0a0ba7d76c7de9905e51ad3fc
MatthiasMatthys <matthys.matthias@hotmail.com>
2024-10-20 13:08:38 +0200

    Delete links on pages

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,22 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  total: number = 0
+  rows: Array<{ raamomtrek: number; aantalRamen: number }> = [
+    { raamomtrek: 0, aantalRamen: 0 } 
+  ];
+
   constructor() {}
 
+  addRow() {
+    this.rows.push({ raamomtrek: 0, aantalRamen: 0 });
+  }
+
+  calculate(){
+    console.log(this.rows);
+    this.rows.forEach(calc => {
+
+      this.total += calc.raamomtrek * calc.aantalRamen;
+    });
+  }
 }
